d7858bba8f3871774efce8814bb6ecac5dbb2a29
Lefkios Paikousis <lefkiospaikousis@yahoo.co.uk>
2020-05-10 16:53:09 +0300

    add parmeters for smouth and correaltion method

--- R/scatter_by_target.R
+++ R/scatter_by_target.R
@@ -4,13 +4,19 @@
 #' @param ... The variables to use. DPLYR style. Supports \code{dplyr::everything()}
 #' @param target The target variable to plot scatter against all other vars
 #' @param width The width of the labeller function in the strips. Defaults t0 25
+#' @param .cor_method The correlation method. One of "pearson", "kendall", spearman". Defaults to "pearson"
+#' @param .smouth The smooth method for the graphs. All option available in the ggplot2::geom_smouth()
 #'
 #' @return A ggplot object
 #' @export
 #'
 #' @examples
 #' lp_scatter_by_target(mtcars, target = mpg, disp:wt)
-lp_scatter_by_target <- function(.data, target = NULL, ...,  width = 25){
+lp_scatter_by_target <- function(.data, target = NULL, ...
+                                 ,  width = 25
+                                 , .cor_method = "pearson"
+                                 , .smouth = "lm"
+                                 ){
 
   # haven;t found a good way to check the inputs here
   #
@@ -24,7 +30,7 @@ lp_scatter_by_target <- function(.data, target = NULL, ...,  width = 25){
 
   df_cor <-
     .data %>%
-    inspectdf::inspect_cor(with_col = rlang::as_label(rlang::enquo(target))) %>%
+    inspectdf::inspect_cor(with_col = rlang::as_label(rlang::enquo(target)), method = .cor_method) %>%
     dplyr::rename("target" = col_1, "variable" = col_2)
 
   #print(df_cor)
@@ -50,7 +56,7 @@ lp_scatter_by_target <- function(.data, target = NULL, ...,  width = 25){
   temp %>%
     ggplot2::ggplot(ggplot2::aes({{target}}, value) ) +
     ggplot2::geom_point( alpha = 0.5 )+
-    ggplot2::geom_smooth(method = "lm")+
+    ggplot2::geom_smooth(method = .smouth)+
     ggplot2::geom_label(ggplot2::aes( x = pos.x
                     , y = pos.y
                     , label = round(corr, 2))
