c1e8cc3baf63a2ec29c9b8619e8eb972e70cd642
Lefkios Paikousis (WST) <lefkiospaik@gmail.com>
2022-07-02 18:31:18 +0300

    update facet_histogram and bar_chart functions

--- R/facet_histogram.R
+++ R/facet_histogram.R
@@ -4,18 +4,25 @@
 #' @param ... The variables to plot. Unquoted, dplyr way
 #' @param statistic One of "mean", "median". Defaults to "mean"
 #' @param .scales Character. The \code{scales} arg of ggplot2::facet_wrap(). One of c("fixed", "free","free_x", "free_y"). Defaults to "fixed"
-#'
+#' @param .colour The colour of the bars. Defaults to "grey80"
 #' @return A ggplot
 #' @export
 #'
 #' @examples
+#' facet_histogram(
+#' mtcars, everything(), .scales = "free", statistic = "median"
+#' )
+#'
 facet_histogram <- function(data, ..., statistic = c("mean", "median"),
-                            .scales = c("fixed", "free","free_x", "free_y")
+                            .scales = c("fixed", "free","free_x", "free_y"),
+                            .colour = "grey80"
                             ) {
 
   statistic <- match.arg(statistic)
   .scales <- match.arg(.scales)
 
+  #TODO Checks
+  #  valid colour
   data_long <-
     data %>%
     dplyr::select(...) %>%
@@ -35,12 +42,12 @@ facet_histogram <- function(data, ..., statistic = c("mean", "median"),
 
   data_long %>%
     ggplot2::ggplot(ggplot2::aes(value))+
-    ggplot2::geom_histogram(colour = "grey80", binwidth = function(x) 2 * IQR(x) / (length(x)^(1/3)))+
+    ggplot2::geom_histogram(colour = .colour,  binwidth = function(x) 2 * IQR(x, na.rm = TRUE) / (length(x)^(1/3)))+
     ggplot2::facet_wrap(~key, scales = .scales)+
     ggplot2::geom_vline( data = data_avg, ggplot2::aes(xintercept = !!sym(statistic)), colour = "red", linetype = "dashed")+
     ggplot2::labs(x = "",
          y = "Frequency",
          caption = paste0("--- Indicates ", statistic, " value"))+
-    ggplot2::theme(strip.text = element_text(size = 12))
+    ggplot2::theme(strip.text = ggplot2::element_text(size = 12))
 
 }
--- R/lp_bar_chart.R
+++ R/lp_bar_chart.R
@@ -2,6 +2,9 @@
 #'
 #' @param data The dataset
 #' @param var The variable to plot
+#' @param .pct_reorder Logical length 1. Reorder the bars based on value?
+#' @param .drop_levels Logical length 1. Drop unused levels in the factor when counting ? i.e. do not show '0' count
+#' @param .hjust Numeric length 1. The jhust for the value labels.
 #' @param .title Optional. The title
 #' @param .xlab The x-axis label. Defaults to the variable name
 #' @param .ylab The y-axis label. Defaults to the variable name
@@ -12,22 +15,37 @@
 #' @export
 #'
 #' @examples
-lp_bar_chart <- function(data, var, .xlab = NULL, .ylab= NULL, .title = NULL, .bar_col = "#EE5C42" ){
+#' lp_bar_chart(iris, Species)
+lp_bar_chart <- function(data, var,
 
-  p <-
+                         .pct_reorder = TRUE,
+                         .drop_levels = TRUE, .hjust = -0.2,
+                         .xlab = NULL, .ylab= NULL, .title = NULL, .bar_col = "#EE5C42" ){
+
+
+  dta_processed <-
     data %>%
-    dplyr::count({{var}}) %>%
+    dplyr::count({{var}}, .drop = .drop_levels) %>%
     dplyr::mutate(pct = n/sum(n),
-           bar_labels = scales::percent(pct,accuracy = 0.1)
+                  bar_labels = scales::percent(pct,accuracy = 0.1)
     ) %>%
-    dplyr::mutate(categories = paste0({{var}}, " (n=", n, ")")) %>%
-    ggplot2::ggplot(ggplot2::aes(x = forcats::fct_reorder(categories, pct), y = pct)) +
+    dplyr::mutate(categories = paste0({{var}}, " (n=", n, ")"))
+
+  if(isTRUE(.pct_reorder)){
+    p <- ggplot2::ggplot(dta_processed,
+                         ggplot2::aes(x = forcats::fct_reorder(categories, pct), y = pct))
+  } else {
+
+    p <- ggplot2::ggplot(dta_processed,
+                         ggplot2::aes(x = forcats::fct_inorder(categories), y = pct))
+  }
+  p <-
+    p +
     ggplot2::geom_bar(stat = "identity", width = 0.5, fill = .bar_col)+
     ggplot2::coord_flip()+
-    ggplot2::geom_text(ggplot2::aes(label = bar_labels), hjust = -0.5)+
-    ggplot2::scale_x_discrete( label = function(x) stringr::str_wrap(x, 30))+
+    ggplot2::geom_text(ggplot2::aes(label = bar_labels), hjust = .hjust)+
+    ggplot2::scale_x_discrete( label = function(x) lp_short(x))+
     ggplot2::scale_y_continuous(expand = ggplot2::expansion(mult = c(0, .1)), limits = c(0,1))+
-    ggplot2::theme_classic(16)+
     ggplot2::theme(
       axis.text.x = ggplot2::element_blank(),
       axis.ticks = ggplot2::element_blank()
@@ -51,5 +69,5 @@ lp_bar_chart <- function(data, var, .xlab = NULL, .ylab= NULL, .title = NULL, .b
 
 
 
-p
+  p
 }
